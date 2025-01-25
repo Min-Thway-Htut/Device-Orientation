c975a570f59851db41751f73cc0554bd6b91cd84
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-01-25 14:10:58 +0100

    Fetching x,y, and z data

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-const ball = document.querySelector(".ball");
-const garden = document.querySelector(".garden");
-const output = document.querySelector(".output");
+window.addEventListener('deviceorientation', (evt) => {
+    const alpha = evt.alpha.toFixed(2);
+    const beta = evt.beta.toFixed(2);
+    const gamma = evt.gamma.toFixed(2);
+    console.log(beta);
+    console.log(alpha);
 
-const maxX = garden.clientWidth - ball.clientWidth;
-const maxY = garden.clientHeight - ball.clientHeight;
-
-function handleOrientation(event) {
-  let x = event.beta; // In degree in the range [-180,180)
-  let y = event.gamma; // In degree in the range [-90,90)
-
-  output.textContent = `beta: ${x}\n`;
-  output.textContent += `gamma: ${y}\n`;
-
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
-  if (x > 90) {
-    x = 90;
-  }
-  if (x < -90) {
-    x = -90;
-  }
-
-  // To make computation easier we shift the range of
-  // x and y to [0,180]
-  x += 90;
-  y += 90;
-
-  // 10 is half the size of the ball
-  // It centers the positioning point to the center of the ball
-  ball.style.left = `${(maxY * y) / 180 - 10}px`; // rotating device around the y axis moves the ball horizontally
-  ball.style.top = `${(maxX * x) / 180 - 10}px`; // rotating device around the x axis moves the ball vertically
-}
-
-window.addEventListener("deviceorientation", handleOrientation);
+    document.getElementById('alpha').textContent = `Alpha Value: ${alpha}`;
+    document.getElementById('beta').textContent = `Beta Value: ${beta}`;
+    document.getElementById('gamma').textContent = `Gamma Value: ${gamma}`;
+})
